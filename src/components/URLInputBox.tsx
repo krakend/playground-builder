950f4e092088a6093db5702d306e2bbf21b8c555
Arpit Bansal <arpit@codalent.com>
2025-01-16 12:13:35 +0530

    cleanup

--- src/components/URLInputBox.tsx
+++ src/components/URLInputBox.tsx
@@ -16,8 +16,6 @@ const URLInputBox: React.FC<URLInputBoxProps> = ({
   isStatic = false,
 }) => {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [buttonText, setButtonText] = useState<string>("View"); // Button text state
-  const [isCopied, setIsCopied] = useState(false); // State to track if the URL is copied
   const [completeUrl, setCompleteUrl] = useState(endpoint || ""); // State to store the complete URL
 
   const handleDivClick = () => {
@@ -32,29 +30,6 @@ const URLInputBox: React.FC<URLInputBoxProps> = ({
       ? `${endpoint.replace(/\/$/, "")}/${inputValue}`
       : inputValue;
     setCompleteUrl(newUrl);
-  };
-
-  const handleCopyUrl = () => {
-    const inputValue = inputRef.current?.value || "";
-
-    // Concatenate the URL and input value
-    const fullUrl = `${endpoint}${inputValue}`;
-
-    // Copy to clipboard
-    navigator.clipboard
-      .writeText(fullUrl)
-      .then(() => {
-        setButtonText("Copied!");
-        setIsCopied(true);
-
-        setTimeout(() => {
-          setButtonText("Copy");
-          setIsCopied(false);
-        }, 2000);
-      })
-      .catch((err) => {
-        console.error("Failed to copy:", err);
-      });
   };
 
   return (
@@ -77,14 +52,12 @@ const URLInputBox: React.FC<URLInputBoxProps> = ({
         </div>
 
         <a
-          className={`bg-brand-neutral-900 text-white rounded-md px-6 py-2 absolute top-2 right-2 flex items-center gap-1 ${
-            isCopied ? "bg-blue-500" : ""
-          }`}
+          className={`bg-brand-neutral-900 text-white rounded-md px-6 py-2 absolute top-2 right-2 flex items-center gap-1`}
           href={completeUrl}
           target="_blank"
           rel="noopener"
         >
-          {buttonText}
+          View
           <ExternalIcon />
         </a>
       </div>
