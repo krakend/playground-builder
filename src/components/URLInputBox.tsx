5aa993e2247ed40e57348af4a5b9d6fc287c05a1
Arpit Bansal <arpit@codalent.com>
2025-01-16 12:05:45 +0530

    feat: refactor URLInputBox for static urls

--- src/components/URLInputBox.tsx
+++ src/components/URLInputBox.tsx
@@ -1,25 +1,37 @@
 "use client";
+import ExternalIcon from "@/image/icons/icon-external.svg";
 import { useRef, useState } from "react";
 
 interface URLInputBoxProps {
   endpoint?: string;
   placeholder?: string;
   helpText?: string;
+  isStatic?: boolean;
 }
 
 const URLInputBox: React.FC<URLInputBoxProps> = ({
   endpoint,
   placeholder,
   helpText,
+  isStatic = false,
 }) => {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [buttonText, setButtonText] = useState<string>("Copy"); // Button text state
+  const [buttonText, setButtonText] = useState<string>("View"); // Button text state
   const [isCopied, setIsCopied] = useState(false); // State to track if the URL is copied
+  const [completeUrl, setCompleteUrl] = useState(endpoint || ""); // State to store the complete URL
 
   const handleDivClick = () => {
     if (inputRef.current) {
       inputRef.current.focus(); // Focus the input
     }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    const newUrl = endpoint
+      ? `${endpoint.replace(/\/$/, "")}/${inputValue}`
+      : inputValue;
+    setCompleteUrl(newUrl);
   };
 
   const handleCopyUrl = () => {
@@ -53,22 +65,28 @@ const URLInputBox: React.FC<URLInputBoxProps> = ({
       >
         <div className="flex text-brand-neutral-600 w-full">
           <p className="shrink-0">{endpoint || ""}</p>
-          <input
-            ref={inputRef}
-            type="text"
-            placeholder={placeholder}
-            className="focus:outline-none w-full"
-          />
+          {!isStatic && (
+            <input
+              ref={inputRef}
+              type="text"
+              placeholder={placeholder}
+              className="focus:outline-none w-full"
+              onChange={handleInputChange}
+            />
+          )}
         </div>
 
-        <button
-          className={`bg-brand-neutral-900 text-white rounded-md px-6 py-2 absolute top-2 right-2 ${
+        <a
+          className={`bg-brand-neutral-900 text-white rounded-md px-6 py-2 absolute top-2 right-2 flex items-center gap-1 ${
             isCopied ? "bg-blue-500" : ""
           }`}
-          onClick={handleCopyUrl}
+          href={completeUrl}
+          target="_blank"
+          rel="noopener"
         >
           {buttonText}
-        </button>
+          <ExternalIcon />
+        </a>
       </div>
       {helpText && (
         <small className="mt-2 text-sm font-normal">{helpText}</small>
